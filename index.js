7204aaa39a86f099f97f6bd780ececef57bebd6d
krishsingla06 <148368716+krishsingla06@users.noreply.github.com>
2026-03-06 16:42:19 +0530

    env

--- index.js
+++ index.js
@@ -14,12 +14,10 @@ dotenv.config();
 
 // Server Configuration
 const app = express();
-const PORT = process.env.PORT || 8000;
-const JWT_SECRET = process.env.JWT_SECRET || "12345";
+const PORT = process.env.PORT ;
+const JWT_SECRET = process.env.JWT_SECRET ;
 const BASE_URL = process.env.FRONTEND_URL || "http://localhost:3000";
-const MONGODB_URI =
-  process.env.MONGODB_URI ||
-  "mongodb+srv://arnavvv:C14hMPSHTpdcB5vq@arnavvv.isvph.mongodb.net/JEE";
+const MONGODB_URI = process.env.MONGODB_URI;
 const DB_NAME = "Jee";
 
 // Create HTTP server
